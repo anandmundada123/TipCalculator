a7db1c7cd93e2cd4df76df5d5bf43728c145ee65
Anand Mundada <amundada@amundada-mn2.linkedin.biz>
2015-01-15 17:33:31 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Anand Mundada on 1/15/15.
+//  Copyright (c) 2015 Anand Mundada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
