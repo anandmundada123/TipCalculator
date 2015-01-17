b9cb5ec9a9ce9ea67000dfa6abe6588b2a9eb3ff
anandmundada123 <anandmundada123@gmail.com>
2015-01-16 17:22:24 -0800

    [ Intro to ios] Project 1 - Review my app

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  TipCalculator
+//
+//  Created by Anand Mundada on 1/16/15.
+//  Copyright (c) 2015 Anand Mundada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
